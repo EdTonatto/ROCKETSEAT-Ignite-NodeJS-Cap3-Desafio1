7b41921e02b4b03d8688bb13ed9c606ad0acc97f
Eduardo Tonatto <edtonatto@gmail.com>
2022-06-13 09:43:30 -0300

    :sparkles: :white_check_mark: Implementação para busca de jogos por titulo contendo uma string

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -14,8 +14,9 @@ export class GamesRepository implements IGamesRepository {
 
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+      .createQueryBuilder('games')
+        .where('UPPER(games.title) like UPPER(:param_title)', {param_title: `%${param}%`})
+        .getMany();
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
